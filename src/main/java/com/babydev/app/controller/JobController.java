7321d99ed9cf7cf54d32f7941e8d2fa93c977bda
alexandraminyov29 <alexandraminyov773@gmail.com>
2023-03-27 00:26:16 +0300

    f

--- src/main/java/com/babydev/app/controller/JobController.java
+++ src/main/java/com/babydev/app/controller/JobController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/jobs")
 @RequiredArgsConstructor
@@ -32,4 +34,10 @@ public class JobController {
     public ResponseEntity<JobPageDTO> getJobById(@RequestParam Long jobId, @RequestParam Long userId) {
         return ResponseEntity.status(HttpStatus.OK).body(jobService.findJobById(jobId, userId));
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Job>> getJobs() {
+        return ResponseEntity.status(HttpStatus.OK).body(jobService.getJobs());
+    }
+
 }
